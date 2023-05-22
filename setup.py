28ceeef73bf2c522856b214da1ede4f331eceecf
aje220 <aje220@exeter.ac.uk>
2023-05-22 13:01:38 +0100

    Faffing with setup things

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-import os
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
-   name='OptiMOBO',
-   version='0.1.0',
+   name='optimobo',
+   version='0.1.2',
    license='MIT',
    description='Solve multi-objective problems using Bayesian optimisation.',
    author='aje220',
    author_email='aje220@exeter.ac.uk',
-   packages=['OptiMOBO'],  #same as name
-   install_requires=[required], #external packages as dependencies
+   packages=['optimobo'],  #same as name
+   install_requires=['numpy>=1.20.0', 'pymoo>=0.6.0', 'pygmo>=2.0.0', 'scipy>=1.9.0', 'scikit-learn>=1.0.0'], #external packages as dependencies
 )
